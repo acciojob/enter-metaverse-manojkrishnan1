d3d1a68c14db2f4c5ce500bbc6afe844d3b2eebe
manojkrishnan1 <127621202+manojkrishnan1@users.noreply.github.com>
2023-09-26 23:34:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let para = document.getElementById('status');
+        
+        // console.log(para.innerHTML);
+        function change(){
+            para.innerHTML = '';
+            let h = document.createElement('h1');
+            h.innerHTML = 'Entered Metaverse';
+            para.append(h);
+		}
